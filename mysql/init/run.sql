b6840d48dd165fbcc4f642fc1cf0308d85118b16
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-02-14 20:45:43 +0530

    Separated: 'build' and 'start' script | Removed: ARGs from docker-compose.yml | updated .gitignore

--- mysql/init/run.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS project_one;
-USE project_one;
-CREATE TABLE IF NOT EXISTS demo (sl_no INT(10) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25) NOT NULL);
-INSERT INTO demo (name) VALUES ROW('Krishnamurty Saha'), ROW('Gopal Krishna Roy'), ROW('Bhairab Guha');
--- scripts/build_and_start.sh
+++ scripts/build_and_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./scripts/setup.sh
 ./scripts/setup-mysql-demo-table.sh
+docker compose build
 docker-compose up -d
