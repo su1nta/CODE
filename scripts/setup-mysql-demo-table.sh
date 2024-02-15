1bed68cc4ef65bfe0d1a64f45de96a70fc1f009b
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-02-15 19:44:05 +0530

    Fixed: init folder not found error

--- scripts/setup-mysql-demo-table.sh
+++ scripts/setup-mysql-demo-table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source php.env
-
+mkdir mysql/init
 echo "CREATE DATABASE IF NOT EXISTS $DATABASE;
 USE $DATABASE;
 CREATE TABLE IF NOT EXISTS demo (sl_no INT(10) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(25) NOT NULL);
